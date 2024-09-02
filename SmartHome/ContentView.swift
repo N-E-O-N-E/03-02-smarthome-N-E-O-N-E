96027230b850bcf99b78d7a96d641a9d9dbdda2c
Markus Wirtz <info@markuswirtz.net>
2024-09-02 12:27:51 +0200

    Datensicherung von NEONE

--- SmartHome/ContentView.swift
+++ SmartHome/ContentView.swift
@@ -6,7 +6,11 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+            
             Text("Hello, world!")
+            
+            
+            
         }
         .padding()
     }
